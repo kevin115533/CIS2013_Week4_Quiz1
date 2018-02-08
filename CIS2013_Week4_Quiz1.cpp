b3169b551a8bf1efcbe07949f7ebbf3b09f026cd
kevin115533 <35547586+kevin115533@users.noreply.github.com>
2018-02-07 19:44:09 -0600

    Edit

--- CIS2013_Week4_Quiz1.cpp
+++ CIS2013_Week4_Quiz1.cpp
@@ -3,24 +3,18 @@ using namespace std;
 
 int main() {
 	int count = 1;
-	char stay;
+	char cont;
 
 	while (count <= 2000) {
-		if ((count % 33 == 0) && (count % 50 == 0)) {
-			cout << count << " FOO BAR" << endl;
-		}
-		else if (count % 33 == 0) {
-			cout << count << " BAR" << endl;
-		}
-		else if (count % 50 == 0) {
+		if (count % 50 == 0)
 			cout << count << " FOO" << endl;
-		}
-		else {
+		else if (count % 33 == 0)
+			cout << count << " BAR" << endl;
+		else
 			cout << count << endl;
-		}
 		count++;
 	}
-	cin >> stay;
+	cin >> cont;
 
 	return 0;
 
